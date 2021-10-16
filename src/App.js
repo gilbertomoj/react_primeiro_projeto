e86d71d9b2b340f9e06945f758e2371a5f1e661f
gilbertomoj <gibamedeirosgc@gmail.com>
2021-10-16 19:59:54 -0300

    Tela de login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="container">
+        <div className="card">
+          <input/>
+          <input/>
+          <span className="btn" onClick={()=>{
+            alert('Realizou o login')
+          }}>
+            Entrar
+          </span>
+          <h6>Esqueci minha senha</h6>
+
+        </div>
+      </header> 
     </div>
   );
 }
